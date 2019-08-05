914e44cb587d3c4e9def65771ecabb7388aea277
Archana <a.archana.singh@gmail.com>
2019-08-04 22:59:24 -0700

    calculator Proj initial change

---
+++ src/component/ButtonPanel.js
@@ -0,0 +1,56 @@
+import Button from "./Button";
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./ButtonPanel.css";
+
+export default class ButtonPanel extends React.Component {
+  static propTypes = {
+    clickHandler: PropTypes.func,
+  };
+
+  handleClick = buttonName => {
+    this.props.clickHandler(buttonName);
+  };
+
+  render() {
+    var style = {
+      border: '3px solid white',
+      'border-radius': '5px'
+    };
+
+    return (
+      <div style={style} className="component-button-panel">
+        <div>
+          <Button name="AC" clickHandler={this.handleClick} />
+          <Button name="+/-" clickHandler={this.handleClick} />
+          <Button name="%" clickHandler={this.handleClick} />
+          <Button name="÷" clickHandler={this.handleClick} blue />
+        </div>
+        <div>
+          <Button name="7" clickHandler={this.handleClick} />
+          <Button name="8" clickHandler={this.handleClick} />
+          <Button name="9" clickHandler={this.handleClick} />
+          <Button name="x" clickHandler={this.handleClick} blue />
+        </div>
+        <div>
+          <Button name="4" clickHandler={this.handleClick} />
+          <Button name="5" clickHandler={this.handleClick} />
+          <Button name="6" clickHandler={this.handleClick} />
+          <Button name="-" clickHandler={this.handleClick} blue />
+        </div>
+        <div>
+          <Button name="1" clickHandler={this.handleClick} />
+          <Button name="2" clickHandler={this.handleClick} />
+          <Button name="3" clickHandler={this.handleClick} />
+          <Button name="+" clickHandler={this.handleClick} blue />
+        </div>
+        <div>
+          <Button name="0" clickHandler={this.handleClick} wide />
+          <Button name="." clickHandler={this.handleClick} />
+          <Button name="=" clickHandler={this.handleClick} blue />
+        </div>
+      </div>
+    );
+  }
+}
